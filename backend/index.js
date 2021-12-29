9c6aec5b3a9640bf8fde21e2c80600b60a795c76
Nikhileshwar VijayaKumar <Nikhileshwar.V@rencata.com>
2021-12-29 17:05:38 +0530

    Changed push notification from Topic subscriptino to device ID target

--- backend/index.js
+++ backend/index.js
@@ -1,31 +1,41 @@
 const express = require('express');
 
-const admin = require("firebase-admin");
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
 
+const admin = require("firebase-admin");
+const serviceAccount = require("./firebaseNotificationKey.json");
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://fir-test-f3ae9.firebaseio.com"
+  });
+const db = getFirestore();
+const docRef = db.collection('devices');
 const app = express();
 
 app.listen('4000', async ()=>{
     console.log("Server listening at http://localhost:4000");
-    var serviceAccount = require("./firebaseNotificationKey.json");
-    admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount),
-        databaseURL: "https://fir-test-f3ae9.firebaseio.com"
-      });
 });
 
-app.get('/', function (req, res){
+app.get('/', async function (req, res) {
     res.send("Visit http://localhost:4000/PushNotification for sending push notification to FCM");
-console.log('Hi there');
 });
 
 app.get('/PushNotification', async function  (req, res)
 {
+    var deviceList = await docRef.get();
+    var tokens = [];
+    deviceList.forEach(element => {
+        data = element.data();
+        tokens.push(data.deviceID);
+    });
+    
     const count= req.query['notification_count'];
     const title= req.query['title'];
     const now = new Date();
     const dateStrings = now.toLocaleString().split(', ');
-    var resp = await admin.messaging().sendToTopic(
-        "Test",
+    var resp = await admin.messaging().sendToDevice(
+        tokens,
         {
             data: {
               title: `${title} ${count}`,
